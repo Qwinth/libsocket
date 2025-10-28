e54282f49de4ca6284e90e4fe160d88ac1117179
Qwinth <76917271+Qwinth@users.noreply.github.com>
2025-10-28 01:18:39 +0100

    Add UDP socket creation for IPv4 and IPv6

---
+++ udp.hpp
@@ -0,0 +1,70 @@
+#pragma once
+#include <stdexcept>
+#include <mutex>
+
+#include <sys/socket.h>
+
+#include "socket.hpp"
+#include "address.hpp"
+#include "common.hpp"
+#include "utils.hpp"
+
+namespace libsocket {
+    namespace ipv4::udp {
+        descriptor socket() {
+            int32_t id = libsocket::utils::random_s32(libsocket::utils::mersenne);
+            uint64_t fingerprint = libsocket::utils::random_u64(libsocket::utils::mersenne);
+
+            libsocket::utils::socket& sock = socket_table.try_emplace(id).first->second;
+
+            sock.fd = ::socket(AF_INET, SOCK_DGRAM, 0);
+            sock.fingerprint = fingerprint;
+
+            sock.working = false;
+            sock.blocking = true;
+            sock.listen = false;
+            sock.accepted = false;
+
+            sock.family = AF_INET;
+            sock.type = SOCK_DGRAM;
+            sock.sockaddr_size = sizeof(sockaddr_in);
+
+            if (sock.fd == -1) {
+                socket_table.erase(id);
+
+                throw std::runtime_error("socket(udp|ipv4): Unable to open socket");
+            }
+            
+            return {id, fingerprint};
+        }
+    }
+
+    namespace ipv6::udp {
+        descriptor socket() {
+            int32_t id = libsocket::utils::random_s32(libsocket::utils::mersenne);
+            uint64_t fingerprint = libsocket::utils::random_u64(libsocket::utils::mersenne);
+
+            libsocket::utils::socket& sock = socket_table.try_emplace(id).first->second;
+
+            sock.fd = ::socket(AF_INET6, SOCK_DGRAM, 0);
+            sock.fingerprint = fingerprint;
+
+            sock.working = false;
+            sock.blocking = true;
+            sock.listen = false;
+            sock.accepted = false;
+
+            sock.family = AF_INET6;
+            sock.type = SOCK_DGRAM;
+            sock.sockaddr_size = sizeof(sockaddr_in6);
+
+            if (sock.fd == -1) {
+                socket_table.erase(id);
+
+                throw std::runtime_error("socket(udp|ipv6): Unable to open socket");
+            }
+            
+            return {id, fingerprint};
+        }
+    }
+}
